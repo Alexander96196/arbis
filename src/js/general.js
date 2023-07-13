5d3b38b3f6223385feff7386a6fa275ff019989a
Alexander96196 <chesnokov.alexander@mail.ru>
2023-07-14 02:37:37 +0500

    intermediate commit

--- src/js/general.js
+++ src/js/general.js
@@ -2,6 +2,23 @@ const toggleNav = document.querySelector(`.header__toggle`);
 const header = document.querySelector(`.header`);
 
 // Меню
-toggleNav.addEventListener("click",  () => {
-    header.classList.toggle(`header--active`)
+toggleNav.addEventListener("click", () => {
+  header.classList.toggle(`header--active`)
+});
+
+// Аккордион
+const accordionItems = document.querySelectorAll('.accordion__item');
+
+accordionItems.forEach(function(item) {
+  const header = item.querySelector('.accordion__header');
+  const content = item.querySelector('.accordion__panel');
+
+  header.addEventListener('click', function() {
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
   });
+});
+
