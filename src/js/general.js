d71c6307101ad2f9acf5ff7e7659fd4f1596e12d
Alexander96196 <chesnokov.alexander@mail.ru>
2023-07-14 17:40:22 +0500

    dev slider

--- src/js/general.js
+++ src/js/general.js
@@ -1,3 +1,5 @@
+const { default: Swiper } = require("swiper");
+
 const toggleNav = document.querySelector(`.header__toggle`);
 const header = document.querySelector(`.header`);
 
@@ -9,11 +11,11 @@ toggleNav.addEventListener("click", () => {
 // Аккордион
 const accordionItems = document.querySelectorAll('.accordion__item');
 
-accordionItems.forEach(function(item) {
+accordionItems.forEach(function (item) {
   const header = item.querySelector('.accordion__header');
   const content = item.querySelector('.accordion__panel');
 
-  header.addEventListener('click', function() {
+  header.addEventListener('click', function () {
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
@@ -22,3 +24,46 @@ accordionItems.forEach(function(item) {
   });
 });
 
+// Инициализация слайдера
+new Swiper('.reviews__slider', {
+  effect: 'fade',
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 100,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    clickable: true,
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+new Swiper('.clients__slider', {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    }
+  },
+});
+
+new Swiper('.articles__slider', {
+  loop: true,
+  // slidesPerView: 2,
+  spaceBetween: 6,
+});
